274c1a623efddbf0ad406297d7352c80a44a0759
JoaoPaulodaSilvaSousa <joaopaulodasilvasousa2021@gmail.com>
2025-05-19 23:40:14 -0300

    update proj-portf

--- Portfolio/script.js
+++ Portfolio/script.js
@@ -1,13 +1,13 @@
-function celular() {
-            if (window.innerWidth >= 768) {
-                itens.style.display = 'block'
-            } else {
-                itens.style.display = 'none'
-            }
-        }
+// function celular() {
+//             if (window.innerWidth <= 768) {
+//                 itens.style.display = 'block'
+//             } else {
+//                 itens.style.display = 'none'
+//             }
+//         }
 
 function clickmenu() {
-    if (itens.style.display = 'block') {
+    if (itens.style.display == 'block') {
         itens.style.display = 'none'
     } else {
         itens.style.display = 'block'
