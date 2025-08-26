6dd7918c68c433cc2b2114f5da776de5aac1ded1
JoaoPaulodaSilvaSousa <joaopaulodasilvasousa2021@gmail.com>
2025-08-26 18:39:21 -0300

    updated arrays

--- exercicios/arrays/arrays_8.js
+++ exercicios/arrays/arrays_8.js
@@ -1,6 +1,26 @@
-let list = []
-for (i = 0; i = 1001; i++) {
-    list[i] = i + 1
+let list = [];
+
+function buscaBinaria(minhaEscolha) {
+    for (let i = 0; i <= 1000; i++) {
+        list[i] = i + 1
+    }
+
+    let menor = 0;
+    let maior = list.length - 1;
+    
+    while (menor <= maior) {
+        const meio = parseInt((menor + maior) / 2);
+
+        let valor = list[meio]
+
+        if (valor === minhaEscolha) {
+            return meio;
+        } if (valor > minhaEscolha) {
+            maior = meio - 1
+        } else {
+            menor = meio + 1
+        }
+    }
 }
 
-console.log(list)
+console.log(buscaBinaria(500));
---
+++ exercicios/arrays/node_ba312d864a71e.tmp.js
@@ -0,0 +1,6 @@
+let list = []
+for (i = 0; i = 1001; i++) {
+    list[i] = i + 1
+}
+
+console.log(list)
