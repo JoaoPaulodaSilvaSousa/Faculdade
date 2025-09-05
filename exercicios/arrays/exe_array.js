5f3cec3ed48891ce217f0b49abea1b4785fdc30a
JoaoPaulodaSilvaSousa <joaopaulodasilvasousa2021@gmail.com>
2025-09-05 17:45:07 -0300

    Update exe_array.js

--- exercicios/arrays/exe_array.js
+++ exercicios/arrays/exe_array.js
@@ -17,19 +17,11 @@ function buscaBinaria(array, escolha) {
     return -1;
 }
 
-let resultado = buscaBinaria(array,);
+//exercicio1 
 
-if (resultado === -1) {
-    console.log(`Valor não encontrado: ${escolha}`);
-} else {
-    console.log(`Valor encontrado. Valor ${escolha}, na posição ${resultado}.`)
+let numeros = [];
+for (let i = 0; i <1000; i++) {
+    numeros[i] = i+1;
 }
 
-
-// Exercício 1: array de 1 a 1000
-let array1 = []
-for (let i = 0; i < 1000; i++) {
-    array1[i] = i+1
-}
-
-console.log(buscaBinaria(array1, 300));
+console.log(buscaBinaria(numeros,300))
