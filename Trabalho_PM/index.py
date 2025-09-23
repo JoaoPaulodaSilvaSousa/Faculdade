bd2530c573f69ae3feae7b523efa0d2293f29750
JoaoPaulodaSilvaSousa <joaopaulodasilvasousa2021@gmail.com>
2025-09-22 23:37:44 -0300

    Update index.py

--- Trabalho_PM/index.py
+++ Trabalho_PM/index.py
@@ -1,32 +1,31 @@
-import os #serve para interagir com o sistema operacional
-import shutil #serve para manipulção de arquivos e pastas
+import os
+import shutil
 
 categorias = {
-    "Documentos": [".txt", ".docx", ".pdf", ],
+    "Documentos": [".txt", ".docx", ".pdf"],
     "Músicas": [".mp3", ".wav"],
     "Imagens": [".jpg", ".jpeg", ".png", ".gif"],
     "Vídeos": [".mp4", ".avi", ".mkv"]
 }
 
-pasta_inicial = os.getcwd() #recebe o diretório atual de onde o programa ta rodando
+pasta_inicial = r"C:\Users\João Paulo\Desktop"
 
-#Listar os arquivos
-for pasta_atual, subpastas, lista_arquivos in os.walk(pasta_inicial): #percorre todos os diretórios e subdiretórios da pasta inicial.
+# Obter caminhos completos das pastas de destino
+pastas_destino = [os.path.join(pasta_inicial, cat) for cat in categorias]
+
+# Listar arquivos
+for pasta_atual, subpastas, lista_arquivos in os.walk(pasta_inicial):
+    # Ignorar pastas de destino
+    subpastas[:] = [p for p in subpastas if os.path.join(pasta_atual, p) not in pastas_destino]
+
     for arquivo in lista_arquivos:
-        caminho_arquivo = os.path.join(pasta_atual, arquivo) #junta o caminho da pasta com o nome do arquivo
-
-        #Pegar  do arquivo
-        _, extensao = os.path.splitext(arquivo) #_ ignora o nome, pois só a extensão é necessária || os.path.splitext erve para separar o nome do arquivo da sua extensão
+        caminho_arquivo = os.path.join(pasta_atual, arquivo)
+        _, extensao = os.path.splitext(arquivo)
 
         for categoria, extensoes in categorias.items():
             if extensao.lower() in extensoes:
-                # Criar pasta se não existir
-                pasta_destino = os.path.join(pasta_inicial, categoria) #Cria o caminho completo da pasta de destino
-
+                pasta_destino = os.path.join(pasta_inicial, categoria)
                 os.makedirs(pasta_destino, exist_ok=True)
-
-                # Mover arquivo
-                shutil.move(caminho_arquivo, os.path.join(pasta_destino, arquivo)) #Move o arquivo para a pasta de destino.
-
+                shutil.move(caminho_arquivo, os.path.join(pasta_destino, arquivo))
                 print(f"Movido: {arquivo} → {categoria}")
-                break # Arquivo já movido, não precisa continuar no loop de categorias
+                break
